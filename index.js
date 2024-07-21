c17e85645253100abdca35c865f53746bf389576
Deepanshu5tiwari <tdeepanshu00@gmail.com>
2024-07-21 20:33:05 +0530

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Hello Javascript');
+console.log('Nice');
+
+num = 61104;
+
+myname = 'Deep';
+
+console.log(myname);
+
+console.log(typeof num);
+console.log(typeof myname);
+
+console.log(1 == '1');
